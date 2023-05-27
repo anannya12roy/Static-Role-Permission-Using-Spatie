a61d4946892b31101d880cd25296b13835b80949
Anannya Roy <roy12anna@gmail.com>
2023-05-27 22:17:41 +0600

    nothing

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -10,11 +10,6 @@ use Spatie\Permission\Models\Role;
 
 class RoleAndPermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
             Permission::create(['name' => 'users']);
